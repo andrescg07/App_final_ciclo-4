86c263e1a022dd6f6f1aff0202bbfd212c65fbe7
JeffHC0911 <jefferhc911@gmail.com>
2021-11-19 15:43:54 -0500

    commit_prueba_3

--- backend/database/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://jeffersonhenao:0911@cluster0.rr3f6.mongodb.net/contratos?retryWrites=true&w=majority', {
-    useNewUrlParser: true
-})
-    .then(db => console.log('Db is connected'))
-    .catch(err => console.log(err));
